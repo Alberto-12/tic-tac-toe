5deae58bfb6f7a7ad22128f147a5f4f02e8690ab
Alberto Serafini <ilgumo@gmail.com>
2022-08-09 16:16:07 -0400

    trying to figure out how to display a icon in the gridCells.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
+//let gridCells = document.querySelectorAll(".squares");
+
+//function gridCellClickHandler() {
+  //for (let i = 0; i < gridCells.length; i++) {
+    //gridCells[i].classList.add("icon-x");
+  //};
+//}
+//gridCells.addEventListener("click", gridCellClickHandler);
+
 let gridCells = document.querySelector(".squares");
+
 function gridCellClickHandler() {
   gridCells.classList.add("icon-x");
 }
+
 gridCells.addEventListener("click", gridCellClickHandler);
+
+let modal = document.getElementById("myModal");
+
+let btn = document.getElementById("myBtn");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+let squares = document.querySelectorAll(".squares");
+ 
+function squaresClickHandler () {
+    squares.classList.add("icon-x");
+  }
+
+for (let i = 0; i < squares.length; i++) {
+squares[i].addEventListener("click", squaresClickHandler); 
+}
+
+
+
+
+
+
