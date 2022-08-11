36e50c97caca57b4b5f504c9cc3c38db79fc12f2
Alberto Serafini <ilgumo@gmail.com>
2022-08-11 16:17:22 -0400

    worked on o combinations and tie combinations

--- js/main.js
+++ js/main.js
@@ -11,14 +11,12 @@ function restartClickHandler() {
 }
 
 let squares = document.querySelectorAll(".squares");
- 
+
 for (let i = 0; i < squares.length; i++) {
-squares[i].addEventListener("click", ); 
+  squares[i].addEventListener("click");
 }
 
-function squaresClickhandler() {
-
-}
+function squaresClickhandler() {}
 
 const nextRoundBtn = document.getElementById("next-round");
 
@@ -104,67 +102,65 @@ if (
 ) {
   alert("Player O wins!");
 } else if (
-    gameGridCells.cell1 === "o" &&
-    gameGridCells.cell2 === "o" &&
-    gameGridCells.cell3 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell4 === "o" &&
-    gameGridCells.cell5 === "o" &&
-    gameGridCells.cell6 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell7 === "o" &&
-    gameGridCells.cell8 === "o" &&
-    gameGridCells.cell9 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell1 === "o" &&
-    gameGridCells.cell4 === "o" &&
-    gameGridCells.cell7 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell2 === "o" &&
-    gameGridCells.cell5 === "o" &&
-    gameGridCells.cell8 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell3 === "o" &&
-    gameGridCells.cell6 === "o" &&
-    gameGridCells.cell9 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell1 === "o" &&
-    gameGridCells.cell5 === "o" &&
-    gameGridCells.cell9 === "o"
-  ) {
-    alert("Player O wins!");
-  } else if (
-    gameGridCells.cell3 === "o" &&
-    gameGridCells.cell5 === "o" &&
-    gameGridCells.cell7 === "o"
-  ) {
-    alert("Player O wins!");
-} else if (
-    gameGridCells.cell1 != "" &&
-    gameGridCells.cell2 != "" &&
-    gameGridCells.cell3 != "" &&
-    gameGridCells.cell4 != "" &&
-    gameGridCells.cell5 != "" &&
-    gameGridCells.cell6 != "" &&
-    gameGridCells.cell7 != "" &&
-    gameGridCells.cell8 != "" &&
-    gameGridCells.cell9 != ""  
+  gameGridCells.cell1 === "o" &&
+  gameGridCells.cell2 === "o" &&
+  gameGridCells.cell3 === "o"
 ) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell4 === "o" &&
+  gameGridCells.cell5 === "o" &&
+  gameGridCells.cell6 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell7 === "o" &&
+  gameGridCells.cell8 === "o" &&
+  gameGridCells.cell9 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell1 === "o" &&
+  gameGridCells.cell4 === "o" &&
+  gameGridCells.cell7 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell2 === "o" &&
+  gameGridCells.cell5 === "o" &&
+  gameGridCells.cell8 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell3 === "o" &&
+  gameGridCells.cell6 === "o" &&
+  gameGridCells.cell9 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell1 === "o" &&
+  gameGridCells.cell5 === "o" &&
+  gameGridCells.cell9 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell3 === "o" &&
+  gameGridCells.cell5 === "o" &&
+  gameGridCells.cell7 === "o"
+) {
+  alert("Player O wins!");
+} else if (
+  gameGridCells.cell1 != "" &&
+  gameGridCells.cell2 != "" &&
+  gameGridCells.cell3 != "" &&
+  gameGridCells.cell4 != "" &&
+  gameGridCells.cell5 != "" &&
+  gameGridCells.cell6 != "" &&
+  gameGridCells.cell7 != "" &&
+  gameGridCells.cell8 != "" &&
+  gameGridCells.cell9 != ""
+) {
+  alert("it's a TIE!");
+}
 
-    alert("it's a TIE!");
-  }
-
-
-  console.log(gameGridCells);
+console.log(gameGridCells);
